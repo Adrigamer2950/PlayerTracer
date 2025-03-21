038aac4c384c9386c5f119c4e34462450ec9110e
Adrigamer2950 <adrigamer2950@gmail.com>
2025-03-21 18:30:46 +0100

    Add warning message when enabling plugin

--- src/main/kotlin/me/adrigamer2950/playerlogs/PlayerLogsPlugin.kt
+++ src/main/kotlin/me/adrigamer2950/playerlogs/PlayerLogsPlugin.kt
@@ -19,11 +19,21 @@ class PlayerLogsPlugin : APIPlugin() {
     override fun onPostLoad() {
         val postLoadTime = System.currentTimeMillis()
 
+        listOf(
+            "&e-------------------------------------------------------",
+            "&eTHIS PLUGIN IS STILL &lUNDER DEVELOPMENT&r&e, SO IT'S",
+            "&e&lNOT&r &eRECOMMENDED TO USE IN PRODUCTION SERVERS",
+            "&eUNLESS YOU KNOW WHAT ARE YOU DOING. WHATEVER",
+            "&eHAPPENS DURING THIS PHASE, IS &lYOUR RESPONSIBILITY&r&e.",
+            "&e-------------------------------------------------------"
+        ).forEach {
+            logger.warn(it)
+        }
+
         logger.info("&aEnabled in ${System.currentTimeMillis() - postLoadTime}ms")
     }
 
     override fun onUnload() {
         logger.info("&cDisabled")
     }
-
 }
