7a904d561495ba70b4332c0e24b52fbfd34b81c6
Adri <business@devadri.es>
2025-09-06 19:56:24 +0200

    Add LogData#displayName

--- api/src/main/kotlin/me/adrigamer2950/playertracer/api/logs/LogData.kt
+++ api/src/main/kotlin/me/adrigamer2950/playertracer/api/logs/LogData.kt
@@ -1,3 +1,7 @@
 package me.adrigamer2950.playertracer.api.logs
 
-class LogData(val id: String, val description: String)
+class LogData(
+    val id: String,
+    val description: String,
+    val displayName: String = id.replaceFirstChar { it.uppercaseChar() }
+)
