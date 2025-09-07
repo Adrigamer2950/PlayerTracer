121de2695bf6249c0ce9b29bcc0100cfe234e032
Adri <business@devadri.es>
2025-09-07 17:27:17 +0200

    Take advantage of Obsidian's PaginatedMenu + Finished log results GUI

--- api/src/main/kotlin/me/adrigamer2950/playertracer/api/location/Location.kt
+++ api/src/main/kotlin/me/adrigamer2950/playertracer/api/location/Location.kt
@@ -1,5 +1,7 @@
 package me.adrigamer2950.playertracer.api.location
 
+import org.bukkit.persistence.PersistentDataAdapterContext
+import org.bukkit.persistence.PersistentDataType
 import kotlin.math.floor
 import kotlin.math.pow
 
@@ -24,6 +26,32 @@ class Location(val worldName: String, x: Double, y: Double, z: Double) {
         private fun truncateDouble(double: Double): Double { // 2 decimals
             val factor = 10.0.pow(2)
             return floor(double * factor) / factor
+        }
+
+        @JvmField
+        val PERSISTENT_DATA_TYPE = object : PersistentDataType<String, Location> {
+            override fun getPrimitiveType(): Class<String> = String::class.java
+
+            override fun getComplexType(): Class<Location> = Location::class.java
+
+            override fun toPrimitive(
+                complex: Location,
+                context: PersistentDataAdapterContext
+            ): String = "${complex.worldName}:${complex.x}:${complex.y}:${complex.z}"
+
+            override fun fromPrimitive(
+                primitive: String,
+                context: PersistentDataAdapterContext
+            ): Location {
+                val l = primitive.split(":")
+
+                val worldName = l[0]
+                val x = l[1].toDoubleOrNull() ?: 0.0
+                val y = l[2].toDoubleOrNull() ?: 0.0
+                val z = l[3].toDoubleOrNull() ?: 0.0
+
+                return Location(worldName, x, y, z)
+            }
         }
     }
 }
--- api/src/main/kotlin/me/adrigamer2950/playertracer/api/logs/LogData.kt
+++ api/src/main/kotlin/me/adrigamer2950/playertracer/api/logs/LogData.kt
@@ -1,7 +1,10 @@
 package me.adrigamer2950.playertracer.api.logs
 
+import me.devadri.obsidian.item.ItemBuilder
+
 class LogData(
     val id: String,
     val description: String,
-    val displayName: String = id.replaceFirstChar { it.uppercaseChar() }
+    val displayName: String = id.replaceFirstChar { it.uppercaseChar() },
+    val guiItem: (ItemBuilder, Log) -> ItemBuilder = { item, _ -> item }
 )
